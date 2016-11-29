0f730dc4839d3cdb67dcaec234a97fd819a5ece9
Woojin Jo <woojin326@naver.com>
2016-11-29 20:33:34 +0900

    2016.11.29 practice update

---
+++ BaekJoon_Algorithm/src/practice/B2742.java
@@ -0,0 +1,14 @@
+package practice;
+
+import java.util.Scanner;
+
+public class B2742 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		for(int i = n; i > 0; i-- ){
+			System.out.println(i);
+		}
+		s.close();
+	}
+}
